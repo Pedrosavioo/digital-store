7de8d984c04df6289a8ec68dcfa3baff530e10f3
Pedrosavioo <pedrosaviolima618@gmail.com>
2024-10-29 22:25:50 -0300

    feat: page selection logic

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
@@ -7,6 +8,7 @@ import Divider from "@mui/material/Divider";
 import MenuIcon from "@mui/icons-material/Menu";
 import "./Drawer.css";
 import Button from "../Button/Button";
+import { useSelected } from "../../context/SelectedContext";
 
 export default function TemporaryDrawer() {
    const [open, setOpen] = React.useState(false);
@@ -15,31 +17,59 @@ export default function TemporaryDrawer() {
       setOpen(!open);
    };
 
+   const { styledSelected, setStyledSelected } = useSelected();
+
    const DrawerList = (
       <Box sx={{ width: 250 }} onClick={toggleDrawer(open)}>
          <List>
             <div id="content-list">
                <div>
                   <h4>Páginas</h4>
-                  <ul>
-                     <li>
-                        <a href="">Home</a>
-                     </li>
-                     <li>
-                        <a href="">Produtos</a>
-                     </li>
-                     <li>
-                        <a href="">Categorias</a>
-                     </li>
-                     <li>
-                        <a href="">Meus Produtos</a>
-                     </li>
-                  </ul>
+                  <nav>
+                     <ul>
+                        <li>
+                           <a
+                              href=""
+                              onClick={() => setStyledSelected(0)}
+                              className={styledSelected === 0 ? "select" : ""}
+                           >
+                              Home
+                           </a>
+                        </li>
+                        <li>
+                           <a
+                              href=""
+                              onClick={() => setStyledSelected(1)}
+                              className={styledSelected === 1 ? "select" : ""}
+                           >
+                              Produtos
+                           </a>
+                        </li>
+                        <li>
+                           <a
+                              href=""
+                              onClick={() => setStyledSelected(2)}
+                              className={styledSelected === 2 ? "select" : ""}
+                           >
+                              Categorias
+                           </a>
+                        </li>
+                        <li>
+                           <a
+                              href=""
+                              onClick={() => setStyledSelected(3)}
+                              className={styledSelected === 3 ? "select" : ""}
+                           >
+                              Meus Produtos
+                           </a>
+                        </li>
+                     </ul>
+                  </nav>
                </div>
                <div id="div-buttons">
-                  <Divider sx={{ marginBottom:'.5rem' }}/>
-                  <Button width="100%" content="entrar" type="primary"/>
-                  <Button width="100%" content="Cadastre-se" type="secondary"/>
+                  <Divider sx={{ marginBottom: ".5rem" }} />
+                  <Button width="100%" content="entrar" type="primary" />
+                  <Button width="100%" content="Cadastre-se" type="secondary" />
                </div>
             </div>
          </List>
@@ -48,7 +78,10 @@ export default function TemporaryDrawer() {
 
    return (
       <div>
-         <MenuIcon onClick={toggleDrawer(open)} sx={{ height: '24px', width: '24px',  cursor: 'pointer' }} />
+         <MenuIcon
+            onClick={toggleDrawer(open)}
+            sx={{ height: "24px", width: "24px", cursor: "pointer" }}
+         />
          <Drawer open={open} onClose={toggleDrawer(open)} id="drawer-mui">
             {DrawerList}
          </Drawer>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
 import "./Header.css";
 import TemporaryDrawer from "../Drawer/Drawer";
@@ -5,10 +6,13 @@ import { Badge, useMediaQuery } from "@mui/material";
 import ShoppingCart from "../../icons/ShoppingCart";
 import Search from "../../icons/Search";
 import Button from "../Button/Button";
+import { useSelected } from "../../context/SelectedContext";
 
 const Header = () => {
    const [openSearch, setOpenSearch] = useState(false);
    const isMobile = useMediaQuery("(max-width: 850px)");
+
+   const { styledSelected, setStyledSelected } = useSelected();
 
    const handleSearchClick = () => {
       setOpenSearch((prev) => !prev);
@@ -44,26 +48,66 @@ const Header = () => {
             </header>
          ) : (
             <header id="header-desktop">
-               <img
-                  src="/assets/icons/logo-header.svg"
-                  alt="Logo Digital Store"
-                  id="img-logo-header"
-               />
-               <div id="input-search-mobile">
-                  <input type="text" placeholder="Pesquisar" />
-                  <Search color="var(--dark-gray-2)" />
-               </div>
-               <div id="div-buttons-desktop">
-                  <div>
-                     <Button width="100px" content="entrar" type="primary" />
-                     <Button content="Cadastre-se" type="secondary" />
+               <div id="div-first-desktop">
+                  <img
+                     src="/assets/icons/logo-header.svg"
+                     alt="Logo Digital Store"
+                     id="img-logo-header"
+                  />
+                  <div id="input-search-mobile">
+                     <input type="text" placeholder="Pesquisar" />
+                     <Search color="var(--dark-gray-2)" />
                   </div>
-                  <Badge badgeContent={4} color="error">
-                     <ShoppingCart
-                        color="var(--primary)"
-                     />
-                  </Badge>
+                  <div id="div-buttons-desktop">
+                     <div>
+                        <Button width="100px" content="entrar" type="primary" />
+                        <Button content="Cadastre-se" type="secondary" />
+                     </div>
+                     <Badge badgeContent={4} color="error">
+                        <ShoppingCart color="var(--primary)" />
+                     </Badge>
+                  </div>
                </div>
+               <nav>
+                  <ul>
+                     <li>
+                        <a
+                           href="#"
+                           onClick={() => setStyledSelected(0)}
+                           className={styledSelected === 0 ? "select" : ""}
+                        >
+                           Home
+                        </a>
+                     </li>
+                     <li>
+                        <a
+                           href="#"
+                           onClick={() => setStyledSelected(1)}
+                           className={styledSelected === 1 ? "select" : ""}
+                        >
+                           Produtos
+                        </a>
+                     </li>
+                     <li>
+                        <a
+                           href="#"
+                           onClick={() => setStyledSelected(2)}
+                           className={styledSelected === 2 ? "select" : ""}
+                        >
+                           Categorias
+                        </a>
+                     </li>
+                     <li>
+                        <a
+                           href="#"
+                           onClick={() => setStyledSelected(3)}
+                           className={styledSelected === 3 ? "select" : ""}
+                        >
+                           Meus Produtos
+                        </a>
+                     </li>
+                  </ul>
+               </nav>
             </header>
          )}
       </>
