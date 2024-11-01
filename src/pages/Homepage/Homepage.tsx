b2335059f4e20498622948dda559ddfbafdcec5f
Pedrosavioo <pedrosaviolima618@gmail.com>
2024-11-01 11:35:56 -0300

    feat: add page homepage

---
+++ src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Homepage.css'
+import Carousel from "../../components/Carousel/Carousel";
+
+const Homepage = () => {
+   return (
+      <section id="homepage">
+         <Carousel />
+      </section>
+   );
+};
+
+export default Homepage;
